cd44ee3fd74b9d2dac5a5473341ba44480b6eab7
Nishant Patil <72811435+nishant219@users.noreply.github.com>
2025-08-20 11:56:15 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,4 +8,4 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>
 );
-//test comment to fix deployment - can be removed
+//test comment to fix deployment - can be removed 
