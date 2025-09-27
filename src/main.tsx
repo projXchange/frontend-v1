99a4e97f0bbd41b3b7c8667d8eae33261da8a532
Amit Pardeshi <amit.pardeshi2000@gmail.com>
2025-09-27 18:12:11 +0530

    add missing import in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-// index.tsx or index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import './index.css';
+
 
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
